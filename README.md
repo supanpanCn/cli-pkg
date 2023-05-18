df0bea59fba5a8ca393c2fbb8fa71c9bf01cddbb
spp <1372978934@qq.com>
2023-05-18 14:06:57 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+# publish-pkg
+
+提供交互式命令发布npm包、生成release和tag
---
+++ tsup.config.ts
@@ -0,0 +1,10 @@
+// tsup.config.ts
+import type { Options } from "tsup";
+export const tsup: Options = {
+  clean: true,
+  format: ["cjs", "esm"],
+  dts: {
+    resolve: true,
+  },
+  entry:['src/index.js']
+};
